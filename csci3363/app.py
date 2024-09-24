9a6a594b75552b0a7a8a70b70f085b2b9cc6471b
ahnsv <ahnsv@bc.edu>
2024-09-24 15:41:00 -0400

    refactor(csci3363): add exception on no data

--- csci3363/app.py
+++ csci3363/app.py
@@ -118,6 +118,8 @@ async def upload_query(file: UploadFile = File(..., )):
 
 @app.get("/current-data")
 async def get_current_data():
+    if not in_memory_datastore:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="No data")
     return JSONResponse(in_memory_datastore[:10])
 
 
